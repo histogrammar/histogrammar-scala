dd31152f03ab2aed74f669b81e8031b5bb945205
Jim Pivarski <jpivarski@gmail.com>
2016-03-30 17:29:44 -0500

    working parser

--- scala/src/main/scala/org/dianahep/histogrammar/json.scala
+++ scala/src/main/scala/org/dianahep/histogrammar/json.scala
@@ -16,29 +16,63 @@ package object json {
   implicit def iterableToJson(x: Iterable[Json]) = JsonArray(x.toSeq: _*)
   implicit def mapToJson(x: Iterable[(JsonString, Json)]) = JsonObject(x.toSeq: _*)
   implicit def mapToJson(x: Map[JsonString, Json]) = JsonObject(x.toSeq: _*)
+
+  private[json] def whitespace(p: ParseState) {
+    while (p.remaining >= 1  &&  (p.get == ' '  ||  p.get == '\t'  ||  p.get == '\n'  ||  p.get == '\r'))
+      p.update(1)
+  }
+
+  def parse(str: String): Option[Json] = {
+    val p = ParseState(str)
+    whitespace(p)
+    val out = Json.parse(p)
+    whitespace(p)
+    if (p.done)
+      out
+    else
+      None
+  }
 }
 
 package json {
-  case class ParseState(str: String, var pos: Int) {
-    private var savePos = 0
-    def save() {savePos = pos}
-    def restore() {pos = savePos}
+  case class ParseState(str: String, var pos: Int = 0) {
+    private var stack = List[Int]()
+    def save() {
+      stack = pos :: stack
+    }
+    def restore() {
+      pos = stack.head
+      stack = stack.tail
+    }
+    def unsave() {
+      stack = stack.tail
+    }
     def get = str(pos)
     def get(n: Int) = str.substring(pos, pos + n)
     def update(n: Int) = {pos += n}
     def remaining = str.size - pos
+    def done = str.size == pos
   }
 
   sealed trait Json
   object Json {
-    def unapply(p: ParseState): Option[Json]
+    def parse(str: String): Option[Json] = parse(ParseState(str))
+    def parse(p: ParseState): Option[Json] =
+      (JsonNull.parse(p) orElse
+        JsonTrue.parse(p) orElse
+        JsonFalse.parse(p) orElse
+        JsonNumber.parse(p) orElse
+        JsonString.parse(p) orElse
+        JsonArray.parse(p) orElse
+        JsonObject.parse(p))
   }
 
   trait JsonPrimitive extends Json
 
   case object JsonNull extends JsonPrimitive {
-    override def toString() = "null"
-    def unapply(p: ParseState) =
+    def stringify = "null"
+    def parse(str: String): Option[JsonNull.type] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonNull.type] =
       if (p.remaining >= 4  &&  p.get(4) == "null") {
         p.update(4)
         Some(JsonNull)
@@ -48,10 +82,18 @@ package json {
   }
 
   trait JsonBoolean extends JsonPrimitive
+  object JsonBoolean {
+    def unapply(x: Json): Option[Boolean] = x match {
+      case JsonTrue => Some(true)
+      case JsonFalse => Some(false)
+      case _ => None
+    }
+  }
 
   case object JsonTrue extends JsonBoolean {
-    override def toString() = "true"
-    def unapply(p: ParseState) =
+    def stringify = "true"
+    def parse(str: String): Option[JsonBoolean] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonBoolean] =
       if (p.remaining >= 4  &&  p.get(4) == "true") {
         p.update(4)
         Some(JsonTrue)
@@ -61,8 +103,9 @@ package json {
   }
 
   case object JsonFalse extends JsonBoolean {
-    override def toString() = "false"
-    def unapply(p: ParseState) =
+    def stringify = "false"
+    def parse(str: String): Option[JsonBoolean] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonBoolean] =
       if (p.remaining >= 5  &&  p.get(5) == "false") {
         p.update(5)
         Some(JsonFalse)
@@ -73,22 +116,33 @@ package json {
 
   trait JsonNumber extends JsonPrimitive
   object JsonNumber {
-    def unapply(p: ParseState) =
-      if (p.remaining >= 1  &&  (('0' <= p.get  &&  p.get <= '9')  ||  p.get == '-')) {
+    def unapply(x: Json): Option[Double] = x match {
+      case JsonInt(y) => Some(y.toDouble)
+      case JsonFloat(y) => Some(y)
+      case _ => None
+    }
+
+    def parse(str: String): Option[JsonNumber] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonNumber] =
+      if (!p.done  &&  (('0' <= p.get  &&  p.get <= '9')  ||  p.get == '-')) {
         p.save()
-        val sb = java.lang.StringBuilder
-        while (p.remaining >= 1  &&  (('0' <= p.get  &&  p.get <= '9')  ||  p.get == '-'  ||  p.get == '+'  ||  p.get == 'e'  ||  p.get == 'E'  ||  p.get == '.')) {
+        val sb = new java.lang.StringBuilder
+        while (!p.done  &&  (('0' <= p.get  &&  p.get <= '9')  ||  p.get == '-'  ||  p.get == '+'  ||  p.get == 'e'  ||  p.get == 'E'  ||  p.get == '.')) {
           sb.append(p.get)
           p.update(1)
         }
         val str = sb.toString
         try {
-          Some(JsonInt(java.lang.Long.parseLong(str)))
+          val out = Some(JsonInt(java.lang.Long.parseLong(str)))
+          p.unsave()
+          out
         }
         catch {
           case _: java.lang.NumberFormatException =>
             try {
-              Some(JsonFloat(java.lang.Double.parseDouble(str)))
+              val out = Some(JsonFloat(java.lang.Double.parseDouble(str)))
+              p.unsave()
+              out
             }
             catch {
               case _: java.lang.NumberFormatException =>
@@ -102,16 +156,18 @@ package json {
   }
 
   case class JsonInt(value: Long) extends JsonNumber {
-    override def toString() = value.toString
+    def stringify = value.toString
   }
 
   case class JsonFloat(value: Double) extends JsonNumber {
-    override def toString() = value.toString
+    def stringify = value.toString
   }
 
   case class JsonString(value: String) extends JsonPrimitive {
+    override def toString() = "JsonString(" + stringify + ")"
+
     // http://stackoverflow.com/a/16652683/1623645
-    override def toString() = {
+    def stringify = {
       val sb = new java.lang.StringBuilder(value.size + 4)
       var t: String = ""
       sb.append('"')
@@ -137,15 +193,16 @@ package json {
     }
   }
   object JsonString {
-    def unapply(p: ParseState) =
-      if (p.remaining >= 1  &&  p.get == '"') {
+    def parse(str: String): Option[JsonString] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonString] =
+      if (!p.done  &&  p.get == '"') {
         p.save()
         val sb = new java.lang.StringBuilder
         p.update(1)
-        while (p.remaining >= 1  &&  p.get != '"') {
+        while (!p.done  &&  p.get != '"') {
           if (p.get == '\\') {
             p.update(1)
-            if (p.remaining >= 1)
+            if (!p.done)
               p.get match {
                 case '"'  => sb.append('"');  p.update(1)
                 case '\\' => sb.append('\\'); p.update(1)
@@ -175,6 +232,7 @@ package json {
         }
         if (p.get == '"') {
           p.update(1)
+          p.unsave()
           Some(JsonString(sb.toString))
         }
         else {
@@ -187,19 +245,119 @@ package json {
   }
 
   trait JsonContainer extends Json
+  object JsonContainer {
+    def unapplySeq(x: Json): Option[Seq[_]] = x match {
+      case JsonArray(elements @ _*) => Some(elements)
+      case JsonObject(pairs @ _*) => Some(pairs)
+      case _ => None
+    }
+  }
 
   case class JsonArray(elements: Json*) extends JsonContainer {
-    override def toString() = "[" + elements.map(_.toString).mkString(", ") + "]"
+    def stringify = "[" + elements.map(_.toString).mkString(", ") + "]"
+  }
+  object JsonArray {
+    def parse(str: String): Option[JsonArray] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonArray] =
+      if (!p.done  &&  p.get == '[') {
+        p.update(1)
+        whitespace(p)
+        if (!p.done  &&  p.get == ']') {
+          p.update(1)
+          return Some(JsonArray())
+        }
+        p.save()
+        val builder = List.newBuilder[Json]
+        var done = false
+        while (!done) {
+          Json.parse(p) match {
+            case Some(x) =>
+              builder += x
+            case None =>
+              p.restore()
+              return None
+          }
+          whitespace(p)
+          if (!p.done  &&  p.get == ',') {
+            p.update(1)
+          }
+          else {
+            done = true
+          }
+          whitespace(p)
+        }
+        if (!p.done  &&  p.get == ']') {
+          p.update(1)
+          p.unsave()
+          Some(JsonArray(builder.result: _*))
+        }
+        else {
+          p.restore()
+          return None
+        }
+      }
+      else
+        None
   }
 
   case class JsonObject(pairs: (JsonString, Json)*) extends JsonContainer {
-    override def toString() = "{" + pairs.map({case (k, v) => k.toString + ": " + v.toString}).mkString(", ") + "}"
+    def stringify = "{" + pairs.map({case (k, v) => k.toString + ": " + v.toString}).mkString(", ") + "}"
   }
-
-  // object parse extends Function1[String, Json] {
-  //   def apply(x: String): Json = {
-  //   }
-  // }
+  object JsonObject {
+    def parse(str: String): Option[JsonObject] = parse(ParseState(str))
+    def parse(p: ParseState): Option[JsonObject] =
+      if (!p.done  &&  p.get == '{') {
+        p.update(1)
+        whitespace(p)
+        if (!p.done  &&  p.get == '}') {
+          p.update(1)
+          return Some(JsonObject())
+        }
+        p.save()
+        val builder = List.newBuilder[(JsonString, Json)]
+        var done = false
+        while (!done) {
+          val key = JsonString.parse(p) match {
+            case Some(x) => x
+            case None =>
+              p.restore()
+              return None
+          }
+          whitespace(p)
+          if (!p.done  &&  p.get == ':')
+            p.update(1)
+          else {
+            p.restore()
+            return None
+          }
+          whitespace(p)
+          Json.parse(p) match {
+            case Some(x) =>
+              builder += (key -> x)
+            case None =>
+              p.restore()
+              return None
+          }
+          whitespace(p)
+          if (!p.done  &&  p.get == ',')
+            p.update(1)
+          else
+            done = true
+          whitespace(p)
+        }
+        if (!p.done  &&  p.get == '}') {
+          p.update(1)
+          p.unsave()
+          Some(JsonObject(builder.result: _*))
+        }
+        else {
+          p.restore()
+          return None
+        }
+      }
+      else
+        None
+  }
 
   package check {
     sealed trait Validity {

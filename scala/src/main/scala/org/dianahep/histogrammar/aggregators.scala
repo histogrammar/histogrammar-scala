a2d6bcb274213f8184d56c71fd1ec1897f57957f
Jim Pivarski <jpivarski@gmail.com>
2016-03-31 10:01:46 -0500

    simplified

--- scala/src/main/scala/org/dianahep/histogrammar/aggregators.scala
+++ scala/src/main/scala/org/dianahep/histogrammar/aggregators.scala
@@ -17,8 +17,8 @@ package histogrammar {
   case class Cache[DOMAIN, RANGE](function: DOMAIN => RANGE) extends Function1[DOMAIN, RANGE] {
     private var last: Option[(DOMAIN, RANGE)] = None
     def apply(x: DOMAIN): RANGE = (x, last) match {
-      case (xref: AnyRef, Some((oldx: AnyRef, oldy))) if ((xref eq oldx)  ||  (xref == oldx)) => oldy
-      case (_, Some((oldx, oldy))) if (x == oldx) => oldy
+      case (xref: AnyRef, Some((oldx: AnyRef, oldy))) if (xref eq oldx) => oldy
+      case (_,            Some((oldx, oldy)))         if (x == oldx)    => oldy
       case _ =>
         val y = function(x)
         last = Some(x -> y)

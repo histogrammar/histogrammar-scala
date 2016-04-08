738b4496fffa6af0d10733d6074c2f50d91238a4
Jim Pivarski <jpivarski@gmail.com>
2016-04-08 09:18:54 -0500

    generalize

--- scala/unevenbin.scala
+++ scala/unevenbin.scala
@@ -1,5 +1,5 @@
-import scala.collection.immutable.TreeSet
 import scala.collection.SortedMap
+import scala.collection.SortedSet
 
 trait MetricOrdering[T] extends Ordering[T] {
   def distance(x: T, y: T): Double
@@ -11,12 +11,12 @@ trait MetricOrdering[T] extends Ordering[T] {
   }
 }
 
-class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrdering[A]) extends SortedMap[A, B] {
+abstract class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrdering[A]) extends SortedMap[A, B] {
   // when the TreeSet searches for an element, keep track of the best distance it finds
   private val best = new java.lang.ThreadLocal[(Double, A, B)]
   best.set((-1.0, null.asInstanceOf[A], null.asInstanceOf[B]))
 
-  private val ord = new MetricOrdering[(A, B)] {
+  protected val ord = new MetricOrdering[(A, B)] {
     def distance(x: (A, B), y: (A, B)) = {
       val diff = ordering.distance(x._1, y._1)
       val absdiff = Math.abs(diff)
@@ -37,7 +37,7 @@ class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrderin
   }
 
   // use a TreeSet as a backing (not TreeMap because we need to get the whole pair back when we query it)
-  private val treeSet = TreeSet[(A, B)](elems: _*)(ord)
+  def treeSet: TreeSet
 
   // find the closest key and return: (distance to key, the key, its associated value)
   def closest(to: A): (Double, A, B) = {
@@ -59,6 +59,28 @@ class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrderin
   def iterator: Iterator[(A, B)] = treeSet.iterator
   def rangeImpl(from: Option[A], until: Option[A]): SortedMap[A, B] = new MetricSortedMap[A, B](treeSet.rangeImpl(from.map((_, null.asInstanceOf[B])), until.map((_, null.asInstanceOf[B]))).toSeq: _*)
 }
+
+// package immutable {
+//   class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrdering[A]) extends MetricSortedMap[A, B](elems: _*)(ordering) {
+//     val treeSet = scala.collection.immutable.TreeSet[(A, B)](elems: _*)(ord)
+//   }
+// }
+
+// package mutable {
+//   class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrdering[A]) extends MetricSortedMap[A, B](elems: _*)(ordering) {
+//     val treeSet = scala.collection.mutable.TreeSet[(A, B)](elems: _*)(ord)
+
+//     def +=(kv: (A, B)): 
+
+    
+//   }
+// }
+
+
+
+
+
+
 
 implicit val doubleOrdering: MetricOrdering[Double] = new MetricOrdering[Double] {
   def distance(x: Double, y: Double) = x - y

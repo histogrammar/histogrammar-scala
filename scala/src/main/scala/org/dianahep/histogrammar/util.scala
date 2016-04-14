c7523334158d7d3c440f3ab93aa9b550b3c4c906
Jim Pivarski <jpivarski@gmail.com>
2016-04-14 12:34:13 -0500

    implemented but have not tested Algorithm 1 of Clustering1D

--- scala/src/main/scala/org/dianahep/histogrammar/util.scala
+++ scala/src/main/scala/org/dianahep/histogrammar/util.scala
@@ -3,11 +3,13 @@ package org.dianahep.histogrammar
 import scala.collection.SortedMap
 import scala.collection.SortedSet
 
-package object util {
-  implicit val doubleOrdering: MetricOrdering[Double] = new MetricOrdering[Double] {
-    def distance(x: Double, y: Double) = x - y
-  }
-}
+import org.dianahep.histogrammar._
+
+// package object util {
+//   implicit val doubleOrdering: MetricOrdering[Double] = new MetricOrdering[Double] {
+//     def distance(x: Double, y: Double) = x - y
+//   }
+// }
 
 package util {
   //////////////////////////////////////////////////////////////// avoid recomputing a function that appears in many histograms
@@ -82,36 +84,71 @@ package util {
   package immutable {
     class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrdering[A]) extends org.dianahep.histogrammar.util.MetricSortedMap[A, B](elems: _*)(ordering) {
       val treeSet = scala.collection.immutable.TreeSet[(A, B)](elems: _*)(ord)
-      // satisfy the contract (I don't care about these methods; could throw UnsupportedOperationException)
+
       def +[B1 >: B](kv: (A, B1)): SortedMap[A, B1] = new MetricSortedMap[A, B](elems :+ (kv._1, kv._2.asInstanceOf[B]): _*)
       def -(key: A): SortedMap[A, B] = new MetricSortedMap[A, B](elems.filter(_._1 != key): _*)
       def get(key: A): Option[B] = treeSet.find(_._1 == key).map(_._2)
       def iterator: Iterator[(A, B)] = treeSet.iterator
-      def rangeImpl(from: Option[A], until: Option[A]): SortedMap[A, B] = new MetricSortedMap[A, B](treeSet.rangeImpl(from.map((_, null.asInstanceOf[B])), until.map((_, null.asInstanceOf[B]))).toSeq: _*)
+      override def size: Int = treeSet.size
+      override def contains(x: A) = treeSet contains (x, null.asInstanceOf[B])
+
+      def rangeImpl(from: Option[A], until: Option[A]): SortedMap[A, B] = throw new UnsupportedOperationException
     }
   }
 
   package mutable {
     class MetricSortedMap[A, B](elems: (A, B)*)(implicit val ordering: MetricOrdering[A]) extends org.dianahep.histogrammar.util.MetricSortedMap[A, B](elems: _*)(ordering) {
       val treeSet = scala.collection.mutable.TreeSet[(A, B)](elems: _*)(ord)
-      def +=(kv: (A, B)): MetricSortedMap[A, B] = {treeSet += kv; this}
-      def -=(elem: A): MetricSortedMap[A, B] = {treeSet -= Tuple2(elem, null.asInstanceOf[B]); this}
-      // satisfy the contract (I don't care about these methods; could throw UnsupportedOperationException)
+
       def +[B1 >: B](kv: (A, B1)): SortedMap[A, B1] = new MetricSortedMap[A, B](elems :+ (kv._1, kv._2.asInstanceOf[B]): _*)
       def -(key: A): SortedMap[A, B] = new MetricSortedMap[A, B](elems.filter(_._1 != key): _*)
       def get(key: A): Option[B] = treeSet.find(_._1 == key).map(_._2)
       def iterator: Iterator[(A, B)] = treeSet.iterator
-      def rangeImpl(from: Option[A], until: Option[A]): SortedMap[A, B] = new MetricSortedMap[A, B](treeSet.rangeImpl(from.map((_, null.asInstanceOf[B])), until.map((_, null.asInstanceOf[B]))).toSeq: _*)
+      override def size: Int = treeSet.size
+      override def contains(x: A) = treeSet contains (x, null.asInstanceOf[B])
+
+      def +=(kv: (A, B)): MetricSortedMap[A, B] = {treeSet += kv; this}
+      def -=(elem: A): MetricSortedMap[A, B] = {treeSet -= Tuple2(elem, null.asInstanceOf[B]); this}
+
+      def rangeImpl(from: Option[A], until: Option[A]): SortedMap[A, B] = throw new UnsupportedOperationException
     }
   }
 
-  //////////////////////////////////////////////////////////////// one-pass adaptive histogram
+  //////////////////////////////////////////////////////////////// 1D clustering algorithm (used by AdaptivelyBin, Median, Percentile)
   // Yael Ben-Haim and Elad Tom-Tov, "A streaming parallel decision tree algorithm",
   // J. Machine Learning Research 11 (2010)
   // http://www.jmlr.org/papers/volume11/ben-haim10a/ben-haim10a.pdf
 
+  class Clustering1D[DATUM, CONTAINER <: Container[CONTAINER] with Aggregation{type Datum = DATUM}](val num: Int, value: => CONTAINER)
+      extends mutable.MetricSortedMap[Double, CONTAINER]()(new MetricOrdering[Double] {def distance(x: Double, y: Double) = x - y}) {
+    def cluster(n: Int) {
+      while (size > n) {
+        val bins = iterator.toList
+        val neighbors = bins.init zip bins.tail
+        val nearestNeighbors = neighbors minBy {case ((x1, v1), (x2, v2)) => x2 - x1}
+
+        val ((x1, v1), (x2, v2)) = nearestNeighbors
+        val replacement = ((x1 * v1.entries + x2 * v2.entries) / (v1.entries + v2.entries), v1 + v2)
+
+        this -= x1
+        this -= x2
+        this += replacement
+      }
+    }
+
+    def update(x: Double, datum: DATUM, weight: Double) {  // "Algorithm 1"
+      get(x) match {
+        case Some(v) => v.fillWeighted(datum, weight)
+        case None =>
+          val v = value   // create a new one
+          v.fillWeighted(datum, weight)
+
+          this += (x, v)
+          cluster(num)
+      }
+    }
 
 
-
+  }
 
 }

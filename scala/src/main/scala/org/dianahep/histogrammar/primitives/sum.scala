3124411068dd30bf097004384335148e4c55c1e6
Jim Pivarski <jpivarski@gmail.com>
2016-04-14 11:26:47 -0500

    added entries to sum

--- scala/src/main/scala/org/dianahep/histogrammar/primitives/sum.scala
+++ scala/src/main/scala/org/dianahep/histogrammar/primitives/sum.scala
@@ -10,63 +10,73 @@ package histogrammar {
     val help = "Accumulate the sum of a given quantity."
     val detailedHelp = """Sum(quantity: NumericalFcn[DATUM], selection: Selection[DATUM] = unweighted[DATUM])"""
 
-    def fixed(value: Double) = new Summed(value)
-    def apply[DATUM](quantity: NumericalFcn[DATUM], selection: Selection[DATUM] = unweighted[DATUM]) = new Summing[DATUM](quantity, selection, 0.0)
+    def fixed(entries: Double, sum: Double) = new Summed(entries, sum)
+    def apply[DATUM](quantity: NumericalFcn[DATUM], selection: Selection[DATUM] = unweighted[DATUM]) = new Summing[DATUM](quantity, selection, 0.0, 0.0)
 
-    def unapply(x: Summed) = Some(x.value)
-    def unapply(x: Summing[_]) = Some(x.value)
+    def unapply(x: Summed) = Some((x.entries, x.sum))
+    def unapply(x: Summing[_]) = Some((x.entries, x.sum))
 
     def fromJsonFragment(json: Json): Container[_] = json match {
-      case JsonFloat(value) => new Summed(value)
+      case JsonObject(pairs @ _*) if (pairs.keySet == Set("entries", "sum")) =>
+        val get = pairs.toMap
+
+        val entries = get("entries") match {
+          case JsonNumber(x) => x
+          case x => throw new JsonFormatException(x, name + ".entries")
+        }
+
+        val sum = get("sum") match {
+          case JsonNumber(x) => x
+          case x => throw new JsonFormatException(x, name + ".sum")
+        }
+
+        new Summed(entries, sum)
+
       case _ => throw new JsonFormatException(json, name)
     }
   }
 
-  class Summed(val value: Double) extends Container[Summed] {
+  class Summed(val entries: Double, val sum: Double) extends Container[Summed] {
     type Type = Summed
-    // type FixedType = Summed
     def factory = Sum
 
-    def zero = new Summed(0.0)
-    def +(that: Summed) = new Summed(this.value + that.value)
+    def zero = new Summed(0.0, 0.0)
+    def +(that: Summed) = new Summed(this.entries + that.entries, this.sum + that.sum)
 
-    // def fix = this
-    def toJsonFragment = JsonFloat(value)
+    def toJsonFragment = JsonObject("entries" -> JsonFloat(entries), "sum" -> JsonFloat(sum))
 
-    override def toString() = s"Summed($value)"
+    override def toString() = s"Summed($sum)"
     override def equals(that: Any) = that match {
-      case that: Summed => this.value === that.value
+      case that: Summed => this.entries === that.entries  &&  this.sum === that.sum
       case _ => false
     }
-    override def hashCode() = value.hashCode
+    override def hashCode() = (entries, sum).hashCode
   }
 
-  class Summing[DATUM](val quantity: NumericalFcn[DATUM], val selection: Selection[DATUM], var value: Double) extends Container[Summing[DATUM]] with AggregationOnData {
+  class Summing[DATUM](val quantity: NumericalFcn[DATUM], val selection: Selection[DATUM], var entries: Double, var sum: Double) extends Container[Summing[DATUM]] with AggregationOnData {
     type Type = Summing[DATUM]
-    // type FixedType = Summed
     type Datum = DATUM
     def factory = Sum
 
-    def zero = new Summing[DATUM](quantity, selection, 0.0)
-    def +(that: Summing[DATUM]) = new Summing(this.quantity, this.selection, this.value + that.value)
+    def zero = new Summing[DATUM](quantity, selection, 0.0, 0.0)
+    def +(that: Summing[DATUM]) = new Summing(this.quantity, this.selection, this.entries + that.entries, this.sum + that.sum)
 
     def fillWeighted[SUB <: Datum](datum: SUB, weight: Double) {
       val w = weight * selection(datum)
       if (w > 0.0) {
         val q = quantity(datum)
-        value += q * w
+        entries += w
+        sum += q * w
       }
     }
 
-    // def fix = new Summed(value)
-    // def toJsonFragment = fix.toJsonFragment
-    def toJsonFragment = JsonFloat(value)
+    def toJsonFragment = JsonObject("entries" -> JsonFloat(entries), "sum" -> JsonFloat(sum))
 
-    override def toString() = s"Summing($value)"
+    override def toString() = s"Summing($sum)"
     override def equals(that: Any) = that match {
-      case that: Summing[DATUM] => this.quantity == that.quantity  &&  this.selection == that.selection  &&  this.value === that.value
+      case that: Summing[DATUM] => this.quantity == that.quantity  &&  this.selection == that.selection  &&  this.entries === that.entries  &&  this.sum === that.sum
       case _ => false
     }
-    override def hashCode() = (quantity, selection, value).hashCode
+    override def hashCode() = (quantity, selection, entries, sum).hashCode
   }
 }

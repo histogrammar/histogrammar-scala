19312a1dee2a678137fa29e70fe59239c8430668
Jim Pivarski <jpivarski@gmail.com>
2016-04-14 12:34:37 -0500

    where adaptive binning aggregator will go

---
+++ scala/src/main/scala/org/dianahep/histogrammar/primitives/adaptivebin.scala
@@ -0,0 +1,12 @@
+package org.dianahep
+
+import org.dianahep.histogrammar.json._
+import org.dianahep.histogrammar.util._
+
+package histogrammar {
+  //////////////////////////////////////////////////////////////// AdaptivelyBin/AdaptivelyBinned/AdaptivelyBinning
+
+
+
+
+}

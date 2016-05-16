966f0d6aba096077ef09f991cc302a2f4cad725b
Jim Pivarski <jpivarski@gmail.com>
2016-05-16 18:39:20 -0400

    basic histogramming works\!

--- scala-sparksql/src/main/scala/org/dianahep/histogrammar/sparksql.scala
+++ scala-sparksql/src/main/scala/org/dianahep/histogrammar/sparksql.scala
@@ -1,21 +1,55 @@
 package org.dianahep.histogrammar
 
+import scala.reflect.ClassTag
+
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.Row
 
 // val df = sqlContext.read.load("taus_from_WW_13TeV_pythia8")
-
+// val hist = Select($"pt" > 20, Bin(20, 0, 4, $"mass" + 1))
 
 package sparksql {
 
 }
 
 package object sparksql {
-  implicit class UserFcnFromColumn[+RANGE](val col: Column) extends UserFcn[, RANGE] {
-
+  implicit class UserFcnFromColumn[+RANGE](var col: Column) extends UserFcn[Row, RANGE] {
+    var index = -1
+    val name = Some(col.toString)
+    def hasCache = false
+    def apply[SUB <: Row](row: SUB): RANGE = row.get(index).asInstanceOf[RANGE]
   }
 
   implicit class DataFrameHistogrammarMethods(df: DataFrame) {
+    def histogrammar[CONTAINER <: Container[CONTAINER] with Aggregation{type Datum = Row} : ClassTag](container: CONTAINER) = {
+      var index = 0
+      val columns = List.newBuilder[Column]
+
+      def gatherColumns(x: Container[_]) {
+        x match {
+          case y: NumericalQuantity[_] => y.quantity match {
+            case z: UserFcnFromColumn[_] =>
+              z.index = index
+              index += 1
+              columns += z.col.cast(DoubleType)
+              z.col = null
+            case _ =>
+              throw new IllegalArgumentException("primitives passed to SQLContext.histogrammar must have spark.sql.Columns for fill rules")
+          }
+
+          // FIXME: handle CategoricalQuantity and AnyQuantity
+
+          case _ => // primitive doesn't have a fill rule
+        }
+
+        x.children.foreach(gatherColumns)
+      }
+      gatherColumns(container)
+
+      df.select(columns.result: _*).rdd.aggregate(container)({(h: CONTAINER, d: Row) => h.fill(d); h}, {(h1: CONTAINER, h2: CONTAINER) => h1 + h2})
+    }
   }
 }

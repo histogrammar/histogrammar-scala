9c2930abee3de28b1340279326fb87c503af7dc3
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-05-23 16:07:33 -0400

    Complete set of conversions to HistogramMethods as in specialized.scala

--- scala-bokeh/src/main/scala/org/dianahep/histogrammar/bokeh.scala
+++ scala-bokeh/src/main/scala/org/dianahep/histogrammar/bokeh.scala
@@ -4,26 +4,45 @@ import scala.language.implicitConversions
 
 import io.continuum.bokeh._
 
-
 package object bokeh extends Tools {
-  implicit def binnedToHistogramMethods(hist: Selected[Binned[Counted, Counted, Counted, Counted]]): HistogramMethods =
+
+  implicit def binnedToHistogramMethods(hist: Binned[Counted, Counted, Counted, Counted]): HistogramMethods =
+    new HistogramMethods(new Selected(hist.entries, None, hist))
+
+  implicit def binningToHistogramMethods[DATUM](hist: Binning[DATUM, Counting, Counting, Counting, Counting]): HistogramMethods =
+    new HistogramMethods(new Selected(hist.entries, None, Factory.fromJson(hist.toJson).as[Binned[Counted, Counted, Counted, Counted]]))
+
+  implicit def selectedBinnedToHistogramMethods(hist: Selected[Binned[Counted, Counted, Counted, Counted]]): HistogramMethods =
     new HistogramMethods(hist)
 
-  implicit def binningToHistogramMethods[DATUM](hist: Selecting[DATUM, Binning[DATUM, Counting, Counting, Counting, Counting]]): HistogramMethods =
+  implicit def selectingBinningToHistogramMethods[DATUM](hist: Selecting[DATUM, Binning[DATUM, Counting, Counting, Counting, Counting]]): HistogramMethods =
     new HistogramMethods(Factory.fromJson(hist.toJson).as[Selected[Binned[Counted, Counted, Counted, Counted]]])
 
-  implicit def sparselyBinnedToHistogramMethods(hist: Selected[SparselyBinned[Counted, Counted]]): HistogramMethods =
-    if (hist.value.numFilled > 0)
+  implicit def sparselyBinnedToHistogramMethods(hist: SparselyBinned[Counted, Counted]): HistogramMethods =
+    if (hist.numFilled > 0)
       new HistogramMethods(
-        new Selected(hist.entries, hist.quantityName, new Binned(hist.value.low.get, hist.value.high.get, 0.0, hist.value.quantityName, hist.value.minBin.get to hist.value.maxBin.get map {i => new Counted(hist.value.at(i).flatMap(x => Some(x.entries)).getOrElse(0L))}, new Counted(0L), new Counted(0L), hist.value.nanflow))
+        new Selected(hist.entries, None, new Binned(hist.low.get, hist.high.get, hist.entries, hist.quantityName, hist.minBin.get to hist.maxBin.get map {i => new Counted(hist.at(i).flatMap(x => Some(x.entries)).getOrElse(0L))}, new Counted(0L), new Counted(0L), hist.nanflow))
       )
     else
       throw new RuntimeException("sparsely binned histogram has no entries")
 
   implicit def sparselyBinningToHistogramMethods[DATUM](hist: Selecting[DATUM, SparselyBinning[DATUM, Counting, Counting]]): HistogramMethods =
-    sparselyBinnedToHistogramMethods(Factory.fromJson(hist.toJson).as[Selected[SparselyBinned[Counted, Counted]]])
+    sparselyBinnedToHistogramMethods(Factory.fromJson(hist.toJson).as[SparselyBinned[Counted, Counted]])
 
-    def plot(xLabel:String, yLabel: String, plots: GlyphRenderer*) : Plot = {
+  implicit def selectedSparselyBinnedToHistogramMethods(hist: Selected[SparselyBinned[Counted, Counted]]): HistogramMethods =
+    if (hist.value.numFilled > 0)
+      new HistogramMethods(
+        new Selected(hist.entries, hist.quantityName, new Binned(hist.value.low.get, hist.value.high.get, hist.value.entries, hist.value.quantityName, hist.value.minBin.get to hist.value.maxBin.get map {i => new Counted(hist.value.at(i).flatMap(x => Some(x.entries)).getOrElse(0L))}, new Counted(0L), new Counted(0L), hist.value.nanflow))
+      )
+    else
+      throw new RuntimeException("sparsely binned histogram has no entries")
+
+  implicit def selectedSparselyBinningToHistogramMethods[DATUM](hist: Selecting[DATUM, SparselyBinning[DATUM, Counting, Counting]]): HistogramMethods =
+    selectedSparselyBinnedToHistogramMethods(Factory.fromJson(hist.toJson).as[Selected[SparselyBinned[Counted, Counted]]])
+  
+
+
+   def plot(xLabel:String, yLabel: String, plots: GlyphRenderer*) : Plot = {
 
       val xdr = new DataRange1d
       val ydr = new DataRange1d
@@ -40,8 +59,8 @@ package object bokeh extends Tools {
       plot
     }
 
-    //allow for default x and y labels 
-    def plot(plots: GlyphRenderer*) : Plot = {
+   //allow for default x and y labels 
+   def plot(plots: GlyphRenderer*) : Plot = {
 
       val xdr = new DataRange1d
       val ydr = new DataRange1d
@@ -56,18 +75,18 @@ package object bokeh extends Tools {
       val children = plots.toList
       plot.renderers := List(xaxis, yaxis):::children
       plot
-    }
+   }
 
-    def save(plot: Plot, fname: String) {
+   def save(plot: Plot, fname: String) {
        val document = new Document(plot)
 
        val html = document.save(fname)
        println(s"Wrote ${html.file}. Open ${html.url} in a web browser.")
        //html.view()
-    }
+   }
 
-    //Method that goes to bokeh-server and plots, so that we can do R-style, ROOT-style plotting
-    def view(document: Document)  = ???
+   //Method that goes to bokeh-server and plots, so that we can do R-style, ROOT-style plotting
+   def view(document: Document)  = ???
 
 }
 

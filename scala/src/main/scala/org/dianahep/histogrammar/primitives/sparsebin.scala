d2cf71ec83d40a70a28d9fca5ec34ae4d19624f3
Jim Pivarski <jpivarski@gmail.com>
2016-06-24 00:26:55 -0500

    remove my name from license

--- scala/src/main/scala/org/dianahep/histogrammar/primitives/sparsebin.scala
+++ scala/src/main/scala/org/dianahep/histogrammar/primitives/sparsebin.scala
@@ -1,4 +1,4 @@
-// Copyright 2016 Jim Pivarski
+// Copyright 2016 DIANA-HEP
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
